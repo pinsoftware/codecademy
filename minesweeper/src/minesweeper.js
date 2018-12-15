601ad190e547c30cd6dc7d3c4c4321b49d50c76f
Dave Pattison <dpattison@missionlinen.com>
2018-12-14 16:42:12 -0800

    printboard

--- minesweeper/src/minesweeper.js
+++ minesweeper/src/minesweeper.js
@@ -1,16 +1,45 @@
 /* jshint esversion: 6*/
-const generatePlayerBoard = (numberofRows, numberOfColumns) => {
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
 	let board = [];
-	for (r = 0; r < numberofRows; r++){
+	for (r = 0; r < numberOfRows; r++){
 		row = [];
 		for (c = 0; c < numberOfColumns; c++){
 			row.push(' ');
 		}
 		board.push(row);
 	}
-	console.log(board);
-}
+	return board;
+};
 
-generatePlayerBoard(3,4);
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+	let board = [];
+	for (r = 0; r < numberOfRows; r++){
+		row = [];
+		for (c = 0; c < numberOfColumns; c++){
+			row.push(null);
+		}
+		board.push(row);
+	}
+	let numberOfBombsPlaced = 0;
+	while (numberOfBombsPlaced <= numberOfBombs){
+		/* need to add logic when a bomb already exists on the space*/
+		const randomRowIndex = Math.floor(Math.random()*numberOfRows);
+		const randomColumnIndex = Math.floor(Math.random()*numberOfColumns);
+		board[randomRowIndex][randomColumnIndex] = 'B';
+		numberOfBombsPlaced++;
+	}
+	return board;
+};
 
+const printBoard = board => { 
+	console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+playerBoard = generatePlayerBoard(3,4);
+bombBoard = generateBombBoard(3,4,5);
+
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
 
